f0ac5b4765b7c15fd6c8314887b5ab1b4c36fbc3
Sime Roncevic <sroncevic19@gmail.com>
2024-01-06 13:11:00 +0100

    Creating functionality for buttons and one hardcoded gif.

--- app/src/main/java/com/example/memepage/RestSoldierActivity.kt
+++ app/src/main/java/com/example/memepage/RestSoldierActivity.kt
@@ -3,38 +3,36 @@ package com.example.memepage
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.memepage.ui.theme.MemePageTheme
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
-class MainActivity : ComponentActivity() {
-
-
+class RestSoldierActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.rest_soldier_activity)
+
+        val gifImageView: ImageView = findViewById(R.id.gifImageView)
+
+        // Load GIF using Glide
+        Glide.with(this)
+            .asGif()
+            .load("https://i.kym-cdn.com/photos/images/newsfeed/002/336/753/653.gif")
+            .into(gifImageView)
 
         val buttonHome: Button = findViewById(R.id.button1)
         val buttonCampfire: Button = findViewById(R.id.button2)
 
         buttonHome.setOnClickListener {
+            // Open Main Activity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
         buttonCampfire.setOnClickListener {
+            // Open Rest Soldier Activity
             val intent = Intent(this, RestSoldierActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
-
-
